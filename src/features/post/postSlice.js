5ec1bf030e0d170e989388fb786e55c429907e9b
Nand2nand <nand28041994@gmail.com>
2023-12-09 12:47:09 +0530

    async-done

--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -12,11 +12,18 @@ const initialState = {
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
     try {
         const response = await axios.get(POSTS_URL);
+        console.log(response)
         return [...response.data]
     } catch (err) {
         return err.message;
     }
 });
+
+export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPost) => {
+    const response = await axios.post(POSTS_URL, initialPost)
+    return response.data
+})
+
 
 const postsSlice = createSlice({
     name:'posts',
@@ -63,7 +70,6 @@ const postsSlice = createSlice({
                 // Adding date and reactions
                 let min = 1;
                 const loadedPosts = action.payload.map(post => {
-                    console.log(post)
                     post.date = sub(new Date(), { minutes: min++ }).toISOString();
                     post.reactions = {
                         thumbsUp: 0,
@@ -82,35 +88,39 @@ const postsSlice = createSlice({
                 state.status = 'failed'
                 state.error = action.error.message
             })
-            // .addCase(addNewPost.fulfilled, (state, action) => {
-            //     // Fix for API post IDs:
-            //     // Creating sortedPosts & assigning the id 
-            //     // would be not be needed if the fake API 
-            //     // returned accurate new post IDs
-            //     const sortedPosts = state.posts.sort((a, b) => {
-            //         if (a.id > b.id) return 1
-            //         if (a.id < b.id) return -1
-            //         return 0
-            //     })
-            //     action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
-            //     // End fix for fake API post IDs 
+            .addCase(addNewPost.fulfilled, (state, action) => {
+                // Fix for API post IDs:
+                // Creating sortedPosts & assigning the id 
+                // would be not be needed if the fake API 
+                // returned accurate new post IDs
+                const sortedPosts = state.posts.sort((a, b) => {
+                    if (a.id > b.id) return 1
+                    if (a.id < b.id) return -1
+                    return 0
+                })
+                action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
+                // End fix for fake API post IDs 
 
-            //     action.payload.userId = Number(action.payload.userId)
-            //     action.payload.date = new Date().toISOString();
-            //     action.payload.reactions = {
-            //         thumbsUp: 0,
-            //         hooray: 0,
-            //         heart: 0,
-            //         rocket: 0,
-            //         eyes: 0
-            //     }
-            //     console.log(action.payload)
-            //     state.posts.push(action.payload)
-            // })
+                action.payload.userId = Number(action.payload.userId)
+                action.payload.date = new Date().toISOString();
+                action.payload.reactions = {
+                    thumbsUp: 0,
+                    wow: 0,
+                    heart: 0,
+                    rocket: 0,
+                    coffee: 0
+                }
+                console.log(action.payload)
+                state.posts.push(action.payload)
+            })
     }
 
 })
 
 export const selectAllPosts = (state) =>  state.posts.posts;
+export const getPostsStatus = (state) =>  state.posts.status;
+export const getPostsError = (state) =>  state.posts.error;
+
+
 export const {postAdded,reactionAdded} = postsSlice.actions;
 export default postsSlice.reducer;
